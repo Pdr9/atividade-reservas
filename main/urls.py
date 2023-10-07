917f0dce87e224a0ee0ce308b7c5a925f45600c6
pdr9 <pedro.segundo@escolar.ifrn.edu.br>
2023-10-06 23:35:12 -0300

    atividade

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import *
+from core.views import list_reserva, create, detail_reserva, delete_reserva
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', listagem, name='listagem'),
-    path('cadastrar/', reserva_criar, name='reserva_criar'),
-    path('detalhes/<int:reserva_id>/', detalhes_reserva, name='detalhes_reserva'),
-    path('excluir/<int:reserva_id>/', excluir_reserva, name='excluir_reserva'),
+    path('', list_reserva, name='listagem'),
+    path('cadastrar/', create, name='reserva_criar'),
+    path('detalhes/<int:reserva_id>/', detail_reserva, name='detalhes_reserva'),
+    path('excluir/<int:reserva_id>/', delete_reserva, name='excluir_reserva'),
 ]
